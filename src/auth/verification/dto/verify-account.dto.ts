d3934da77f433fc0f63692e44e868d45c190d5bb
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2024-08-26 18:46:10 +0100

    fix: remove email from reset password

--- src/auth/verification/dto/verify-account.dto.ts
+++ src/auth/verification/dto/verify-account.dto.ts
@@ -2,10 +2,6 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class VerifyAccountDto {
   @IsNotEmpty()
-  @IsEmail()
-  email: string;
-
-  @IsNotEmpty()
   @MinLength(6)
   verificationCode: string;
 }
